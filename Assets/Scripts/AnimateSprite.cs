e31f2ae2face2467c4cc3c09d1548efb6311e0d3
Vincent Hubau <vincent.hubau@gmail.com>
2015-01-19 23:13:36 -0500

    Beginning of the world
    Set a small area and the basics for the combat area.
    Character moves as it should, from a point to another on a virtual grid.
    3 types of tiles that need tweaking (trees don't collide because no
    collider on the player at the moment).
    Combat begins and goes to the combat screen.

---
+++ Assets/Scripts/AnimateSprite.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateSprite : MonoBehaviour {
+	public int uvAnimationTileX = 3;
+	public int uvAnimationTileY = 4;
+	public float framesPerSecond = 10.0f;
+
+	// Update is called once per frame
+	void Update () {
+		int index = (int)(Time.time * framesPerSecond);
+		index = index % (4 * 4);
+		Vector2 size = new Vector2 (1.0f / uvAnimationTileX , 1.0f / uvAnimationTileY);
+		float uIndex = index % uvAnimationTileX;
+		float vIndex = index / uvAnimationTileX;
+		Vector2 offset = new Vector2 (uIndex * size.x , 1.0f - size.y + vIndex * size.y);
+		renderer.material.SetTextureOffset("_MainTex", offset);
+		renderer.material.SetTextureScale("_MainTex", size);
+	}
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	//Moving stats
+	public float speed;
+	Vector3 startPoint;
+	Vector3 endPoint;
+	bool isMoving;
+	float increment;
+
+	//Walking Stats
+	int walkCounter;
+	int walkCounter2;
+	int minWC2, maxWC2;
+	bool isInCombat;
+
+	//Cameras
+	public GameObject cameraMain;
+	public GameObject cameraCombat;
+
+	// Use this for initialization
+	void Start () {
+		startPoint = transform.position;
+		endPoint = transform.position;
+
+		minWC2 = Random.Range( 5, 25 );
+		maxWC2 = Random.Range( 25, 50 );
+		walkCounter2 = Random.Range ( minWC2, maxWC2 );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Movement
+
+		if(increment <= 1 && isMoving == true){
+			increment += speed /100;
+		}
+		else{
+			isMoving = false;
+		}
+
+		if(isMoving){
+			transform.position = Vector3.Lerp(startPoint, endPoint, increment);
+		}
+
+		if(!isInCombat){
+			if(Input.GetKey("z") && isMoving == false){
+				CalculateWalk();
+				increment = 0;
+				isMoving = true;
+				startPoint = transform.position;
+				endPoint = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+			}
+			else if(Input.GetKey("s") && isMoving == false){
+				CalculateWalk();
+				increment = 0;
+				isMoving = true;
+				startPoint = transform.position;
+				endPoint = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+			}
+			else if(Input.GetKey("d") && isMoving == false){
+				CalculateWalk();
+				increment = 0;
+				isMoving = true;
+				startPoint = transform.position;
+				endPoint = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+			}
+			else if(Input.GetKey("q") && isMoving == false){
+				CalculateWalk();
+				increment = 0;
+				isMoving = true;
+				startPoint = transform.position;
+				endPoint = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+			}
+		}
+	}
+
+	//Test if the number of steps as reached the limit before entering a fight
+	void CalculateWalk(){
+		if(walkCounter >= walkCounter2){
+			minWC2 = Random.Range( 5, 25 );
+			maxWC2 = Random.Range( 25, 50 );
+			walkCounter2 = Random.Range(minWC2, maxWC2);
+			walkCounter = 0;
+			EnterCombat();
+		}
+		else{
+			//if in tall grass
+			walkCounter++;
+		}
+	}
+
+	void EnterCombat(){
+		cameraMain.SetActive(false);
+		cameraCombat.SetActive(true);
+		isInCombat = true;
+		Debug.Log("Entering fight");
+	}
+}
